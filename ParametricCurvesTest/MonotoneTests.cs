3485e958d15439e53707b6de611d514535e45e6c
Osama Kawish <osamakawish@gmail.com>
2024-07-08 21:01:10 -0400

    Modified folder layout. Includes test project now.

---
+++ ParametricCurvesTest/MonotoneTests.cs
@@ -0,0 +1,13 @@
+using ParametricCurves;
+
+namespace ParametricCurvesTest;
+
+[TestClass]
+public class MonotoneTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Monotone monotone;
+    }
+}
